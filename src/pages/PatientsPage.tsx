df57bfb8ce9411bbd9d66dceb6359d2c633899a0
Diego <devgo.bydrgm21@gmail.com>
2024-12-04 12:08:16 -0300

    Refactor

--- src/pages/PatientsPage.tsx
+++ src/pages/PatientsPage.tsx
@@ -1,14 +1,9 @@
-import { usePatient } from "../hooks/usePatient"
 
 function PatientsPage() {
 
-  const { patients } = usePatient()
-
-  console.log(patients)
-
   return (
     <div>
-      <h1>Patient Page</h1>
+      
     </div>
   )
 }
